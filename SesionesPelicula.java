18e91d555cf2235220632d56f8f6c2c02de3250a
NicolasMoya <nicolasmvalenzuela@gmail.com>
2016-08-23 18:23:36 -0300

    Add files via upload

---
+++ SesionesPelicula.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author lab
+ */
+class SesionesPelicula {
+    Pelicula pelicula;
+    Vector SesionesStrPelicula;
+
+    public SesionesPelicula(Pelicula pelicula, Vector sesionesStrPelicula) {
+    }
+    
+}

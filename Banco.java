2d09c7e799c9726f8326e4c8cfc5a2cb935d2c85
NicolasMoya <nicolasmvalenzuela@gmail.com>
2016-08-26 12:57:54 -0300

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -6,20 +6,27 @@ public class Banco {
 	private String Nombre;
 	private String Direccion;
 	private int Telefono;
-
+        private Cajero cajero[];
+        private Cuenta cuenta[];
+            
+        Cuenta cu = new Cuenta();
 	public void verificarTransaccion() {
 		// TODO - implement Banco.verificarTransaccion
 		throw new UnsupportedOperationException();
 	}
 
 	public void VerificarCuenta() {
-		// TODO - implement Banco.VerificarCuenta
-		throw new UnsupportedOperationException();
+	int numcuent;
+        int numclient;
+                
+            System.out.println("\nsu cuenta : \nnumero de cuenta :"+cu.getNumCuenta()+ 
+                    "\nNumero de cliente : " + cu.getNumCliente() + "\n");
+            
 	}
 
 	public void VerificarSaldo() {
-		// TODO - implement Banco.VerificarSaldo
-		throw new UnsupportedOperationException();
+		
+                System.out.println("su saldo es : " + cu.getSaldo());
 	}
 
 }

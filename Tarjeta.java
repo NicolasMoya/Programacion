c09c8d6b80d35e426778ae90d2efcc53a9e11927
NicolasMoya <nicolasmvalenzuela@gmail.com>
2016-08-26 13:01:12 -0300

    Update Tarjeta.java

--- Tarjeta.java
+++ Tarjeta.java
@@ -1,6 +1,7 @@
 package cajero;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Tarjeta {
 
@@ -8,13 +9,46 @@ public class Tarjeta {
 	private int NumCuenta;
 	private int NumCliente;
 	private Date FechadeExpiracion;
+        private Cuenta Cuenta;
 
-    public Tarjeta(int NumTarjeta, int NumCuenta, int NumCliente, Date FechadeExpiracion) {
+    public Tarjeta(int NumTarjeta, int NumCuenta, int NumCliente, Date FechadeExpiracion,Cuenta cuenta) {
         this.NumTarjeta = NumTarjeta;
         this.NumCuenta = NumCuenta;
         this.NumCliente = NumCliente;
         this.FechadeExpiracion = FechadeExpiracion;
+        this.Cuenta = Cuenta;
     }
+
+    public int getNumTarjeta() {
+        return NumTarjeta;
+    }
+
+
+    public int getNumCuenta() {
+        return NumCuenta;
+    }
+
+    public void setNumCuenta(int NumCuenta) {
+        this.NumCuenta = NumCuenta;
+    }
+
+    public int getNumCliente() {
+        return NumCliente;
+    }
+
+    public void setNumCliente(int NumCliente) {
+        this.NumCliente = NumCliente;
+    }
+
+    public Date getFechadeExpiracion() {
+        return FechadeExpiracion;
+    }
+    public void setFechadeExpiracion(){
+        
+        
+    }
+
+   
         
         
         

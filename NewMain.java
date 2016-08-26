01f9f6965b0a74fe6671b53acdc7b0f35c724247
NicolasMoya <nicolasmvalenzuela@gmail.com>
2016-08-26 13:02:57 -0300

    Add files via upload

---
+++ NewMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajero;
+
+import java.util.Random;
+
+/**
+ *
+ * @author lab
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random  rnd = new Random();
+       
+        
+        Cliente cli1 = new Cliente(1,2,"Nicolas","Hola",123585897);
+        
+        
+        
+        
+        
+    }
+    
+}
